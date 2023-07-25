538613d5645b5f981984a3f3b1146822ff3dcc85
PabloDAcevedo <pablo.ace.0123@gmail.com>
2023-07-25 16:48:49 -0300

    prueba, no se de que

--- Pruebas/ejercicios/ejercicio_de_prueba.py
+++ Pruebas/ejercicios/ejercicio_de_prueba.py
@@ -1,4 +1,13 @@
-lista_a = ["a", 3, "hola"]
-lista_b = [55, 66, 77]
-lista_a.append(lista_b)
-print(lista_a)
+resultado=True
+dato = int(input("valor"))
+divisor = 2
+
+while((divisor < dato) and resultado):
+    if (dato % divisor == 0):
+        resultado = False
+divisor = divisor + 1
+
+if (resultado):
+    print("Si, es!")
+else: 
+    print("No, no es!")
